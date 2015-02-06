5fd90851454d4da960efcd6be9f705796e3c19d1
chengzh2008 <chengzh2008@gmail.com>
2015-02-05 18:19:06 -0800

    asycn working

--- index.js
+++ index.js
@@ -4,45 +4,65 @@ var bitmap = require('./lib/bitmap'),
     path = require('path'),
     fs = require('fs'),
     myApp = function () {
-        var fileName = process.argv[2],
+        console.log(process.argv);
+        var fileName = process.argv[3],
+            transformTo = process.argv[2],
             outputFileName,
             buf, metaInfo, palette,
             count, currentPalette,
             inputFileInfo,
-            basename, extension;
+            basename, extension,
+            transformFn;
+        transformTo = transformTo.toLowerCase();
+        if (transformTo === "invert") {
+            transformFn = bitmap.invert;
+        } else if (transformTo === "grayscale") {
+            transformTo = bitmap.grayscale;
+        } else if (transformTo === "scale") {
+            transformFn = bitmap.scale;
+        }
+
 
         if (!fileName) {
             console.log("please give a picture to transform");
             return;
         }
 
-        buf = fs.readFileSync(fileName);
 
-        metaInfo = bitmap.metaReader(buf);
-        palette = bitmap.getPaletteArray(buf);
-        //console.log(pixelData.length);
-
-        for (count = 0; count < palette.length; count++) {
-            currentPalette = palette.slice(count * 4, count * 4 + 4);
-            //console.log(currentPixel);
-            bitmap.invert(currentPalette); // invert each pixel
-            //console.log(currentPixel);
-        }
-        console.log(count);
-
-        // put modified pixelData into original buffer
-        palette.copy(buf, 54, 0, palette.length);
-
-
-        // save buf to a file
-        extension = '.' + fileName.split('.')[1];
-        basename = path.basename(fileName, extension);
-        outputFileName = basename + "-inverted" + extension;
-        fs.writeFile(outputFileName, buf, function (err) {
+        fs.readFile(fileName, function (err, buf) {
             if (err) {
                 throw err;
             }
+
+            metaInfo = bitmap.metaReader(buf);
+            palette = bitmap.getPaletteArray(buf);
+            //console.log(pixelData.length);
+
+            for (count = 0; count < palette.length; count++) {
+                currentPalette = palette.slice(count * 4, count * 4 + 4);
+                //console.log(currentPixel);
+                transformFn(currentPalette); // invert each pixel
+                //console.log(currentPixel);
+            }
+
+            // put modified pixelData into original buffer
+            palette.copy(buf, 54, 0, palette.length);
+
+
+            // save buf to a file
+            extension = '.' + fileName.split('.')[1];
+            basename = path.basename(fileName, extension);
+            outputFileName = basename + "-" + transformTo + extension;
+            fs.writeFile(outputFileName, buf, function (err) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            console.log("done with the transform!")
+
         });
+
     };
 
 myApp();
--- test/myAppTest.js
+++ test/myAppTest.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var myApp = require('../index'),
-    expect = require('chai').expect;
+    expect = require('chai').expect,
+    fs = require('fs');
 
 describe('myApp from index.js', function () {
     var arg_cache;
@@ -9,15 +10,20 @@ describe('myApp from index.js', function () {
     before(function () {
         // cache the current process.argv
         arg_cache = process.argv;
-        process.argv = ['node', 'index.js', 'test.bmp'];
+        process.argv = ['node', 'index.js', 'invert', 'test.bmp'];
+
+    });
+
+
+    it('should take right filename', function () {
+        console.log(process.argv);
+        myApp();
+        console.log(process.argv);
+        //expect(fs.existsSync(test-invert.bmp)).to.be.true;
     });
 
     // restore the process.argv
     after(function () {
         process.argv = arg_cache;
-    });
-
-    it('should take right filename', function () {
-
     });
 });
