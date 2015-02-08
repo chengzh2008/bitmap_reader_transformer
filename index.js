3865ba8b8ecf219f26d5bac204ca72e638e81339
chengzh2008 <chengzh2008@gmail.com>
2015-02-07 23:16:43 -0800

    modify test code

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 "use strict";
 var myApp = require('./lib/transformer'),
     fileName = process.argv[3],
-    transformTo = process.argv[2];
+    transformTo = process.argv[2],
+    options = ['invert', 'grayscale', 'scale'];
 
 // check for command line input validity
-if (!transformTo in ['invert', 'grayscale', 'scale']) {
+
+if (options.indexOf(transformTo) < 0) {
     console.log("invalid transform request");
-    return;
+    process.exit(1);
 }
 
 if (!fileName) {
     console.log("please give a picture to transform");
-    return;
+    process.exit(1);
 }
 
 // do the job
---
+++ test/transformerTest.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var transformer = require('../lib/transformer'),
+    expect = require('chai').expect,
+    path = require('path'),
+    fs = require('fs');
+
+describe('myApp from transformer.js', function () {
+
+    it('should get right output filename based in input', function () {
+        var filename = "abc.bmp",
+            transformTo = "invert";
+        expect(transformer.getOutputFilename(filename, transformTo)).to.be.eql("abc-invert.bmp");
+    });
+
+    it('should save buf to file', function (done) {
+        var buf = new Buffer(20),
+            outputFileName = 'saved.bmp';
+        transformer.saveBufToFile(outputFileName, buf, function () {
+            fs.exists("saved.bmp", function (isThere) {
+                expect(isThere).to.be.eql(true);
+                done();
+            });
+        });
+    });
+
+});
