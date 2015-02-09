7294925aea17a2478d92faf54b9bfb656bf24efe
chengzh2008 <chengzh2008@gmail.com>
2015-02-08 21:24:02 -0800

    final version

--- index.js
+++ index.js
@@ -2,22 +2,33 @@
 "use strict";
 var myApp = require('./lib/transformer'),
     fileName = process.argv[3],
-    transformTo = process.argv[2],
-    options = ['invert', 'grayscale', 'scale'];
+    format = process.argv[2],
+    options = ['invert', 'grayscale', 'scale'],
+    transformTo, factor;
 
-// check for command line input validity
+// get input from command line
+if (format.indexOf(':') >= 0) {
+    format = format.split(":");
+    transformTo = format[0];
+    factor = parseInt(format[1], 10);
+} else {
+    transformTo = format;
+}
 
+// check input is valid or not
 if (options.indexOf(transformTo) < 0) {
     console.log("invalid transform request");
     process.exit(1);
 }
 
+// check if the picture is provided or not
 if (!fileName) {
     console.log("please give a picture to transform");
     process.exit(1);
 }
 
-// do the job
-myApp.transform(fileName, transformTo);
+// do the image transformation
+console.log("Doing the transformation...");
+myApp.transform(fileName, transformTo, factor);
 
 module.exports = myApp;
--- test/transformerTest.js
+++ test/transformerTest.js
@@ -9,6 +9,13 @@ describe('myApp from transformer.js', function () {
 
     it('should get right output filename based in input', function () {
         var filename = "abc.bmp",
+            transformTo = "scale",
+            factor = 30;
+        expect(transformer.getOutputFilename(filename, transformTo, factor)).to.be.eql("abc-scale-30.bmp");
+    });
+
+    it('should get right output filename based in input', function () {
+        var filename = "abc.bmp",
             transformTo = "invert";
         expect(transformer.getOutputFilename(filename, transformTo)).to.be.eql("abc-invert.bmp");
     });
