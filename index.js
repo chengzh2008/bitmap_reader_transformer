0be0a8bfbd14c45c2b528d555f19bd9428d245b0
chengzh2008 <chengzh2008@gmail.com>
2015-02-08 22:05:46 -0800

    enable color selection for scale transformation

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ var myApp = require('./lib/transformer'),
     fileName = process.argv[3],
     format = process.argv[2],
     options = ['invert', 'grayscale', 'scale'],
-    transformTo, factor;
+    rgb, transformTo, factor;
 
 // get input from command line
 if (format.indexOf(':') >= 0) {
     format = format.split(":");
-    transformTo = format[0];
-    factor = parseInt(format[1], 10);
+    if (format.length === 3) {
+        rgb = format[0];
+        transformTo = format[1];
+        factor = parseInt(format[2], 10);
+    } else {
+        transformTo = format[0];
+        factor = parseInt(format[1], 10);
+
+    }
 } else {
     transformTo = format;
 }
+
+console.log(rgb, transformTo, factor);
 
 // check input is valid or not
 if (options.indexOf(transformTo) < 0) {
@@ -29,6 +38,6 @@ if (!fileName) {
 
 // do the image transformation
 console.log("Doing the transformation...");
-myApp.transform(fileName, transformTo, factor);
+myApp.transform(fileName, transformTo, factor, rgb);
 
 module.exports = myApp;
