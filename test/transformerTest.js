66b3e50a192d98aeb5aad0d0bc6a8d88a752248f
chengzh2008 <chengzh2008@gmail.com>
2015-02-08 22:06:42 -0800

    enable color selection for scale transformation

--- test/transformerTest.js
+++ test/transformerTest.js
@@ -10,14 +10,21 @@ describe('myApp from transformer.js', function () {
     it('should get right output filename based in input', function () {
         var filename = "abc.bmp",
             transformTo = "scale",
+            rgb = "blue",
             factor = 30;
-        expect(transformer.getOutputFilename(filename, transformTo, factor)).to.be.eql("abc-scale-30.bmp");
+        expect(transformer.getOutputFilename(filename, transformTo, factor, rgb)).to.be.eql("abc-blue-scale-30.bmp");
     });
 
     it('should get right output filename based in input', function () {
         var filename = "abc.bmp",
             transformTo = "invert";
         expect(transformer.getOutputFilename(filename, transformTo)).to.be.eql("abc-invert.bmp");
+    });
+
+    it('should get right output filename based in input', function () {
+        var filename = "abc.bmp",
+            transformTo = "grayscale";
+        expect(transformer.getOutputFilename(filename, transformTo)).to.be.eql("abc-grayscale-10.bmp");
     });
 
     it('should save buf to file', function (done) {
